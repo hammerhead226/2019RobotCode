c280d7ffc9fd18926aaba0253c277c38ff360f66
njaison <nidhijaison@gmail.com>
2019-02-05 19:03:44 -0500

    Organized constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,73 +11,64 @@ package frc.robot;
  * Add your docs here.
  */
 public class Constants {
-    public static final double DT_VOLTAGE_LIMIT = 11;
-    public static final boolean DT_VOLTAGE_LIMIT_ENABLED = true;
 
-    public static final double INTAKE_VOLTAGE_LIMIT = 11;
-    public static final boolean INTAKE_VOLTAGE_LIMIT_ENABLED = true;
-
-    public static final double ARM_VOLTAGE_LIMIT = 11;
-    public static final boolean ARM_VOLTAGE_LIMIT_ENABLED = true;
-
-    public static final int DT_CURRENT_LIMIT = 30;
-    public static final boolean DT_CURRENT_LIMIT_ENABLED = false;
-
-    public static final int INTAKE_CURRENT_LIMIT = 30;
-    public static final boolean INTAKE_CURRENT_LIMIT_ENABLED = false;
-
-    public static final int ARM_CURRENT_LIMIT = 30;
-    public static final boolean ARM_CURRENT_LIMIT_ENABLED = false;
-
+    //Inverts
     public static final boolean DT_INVERT_L = false;
     public static final boolean DT_INVERT_R = false;
-
     public static final boolean INTAKE_INVERT_L = false;
     public static final boolean INTAKE_INVERT_R = false;
-
     public static final boolean ARM_INVERT_L = false;
     public static final boolean ARM_INVERT_R = false;
-
-    public static final int DT_LEFT_PIDSLOT_IDX = 0;
-    public static final int DT_RIGHT_PIDSLOT_IDX = 0;
-
-    public static final int INTAKE_LEFT_PIDSLOT_IDX = 0;
-    public static final int INTAKE_RIGHT_PIDSLOT_IDX = 0;
-
-    public static final int ARM_LEFT_PIDSLOT_IDX = 0;
-    public static final int ARM_RIGHT_PIDSLOT_IDX = 0;
-
-    public static final int DT_TIMEOUT = 10;
-
-    public static final int INTAKE_TIMEOUT = 10;
-
-    public static final int ARM_TIMEOUT = 10;
-
-    public static final boolean DT_LEFT_SENSOR_PHASE = true;
-    public static final boolean DT_RIGHT_SENSOR_PHASE = true;
-
-    public static final boolean ARM_LEFT_SENSOR_PHASE = true;
-    public static final boolean ARM_RIGHT_SENSOR_PHASE = true;
-
-    public static final double DT_VOLTAGE_RAMP_RATE = 0.1;
-  
-    public static final double EL_VOLTAGE_LIMIT = 11;
-    public static final boolean EL_VOLTAGE_LIMIT_ENABLED = true;
-
-    public static final int EL_CURRENT_LIMIT = 30;
-    public static final boolean EL_CURRENT_LIMIT_ENABLED = false;
-
-    public static final int EL_PIDSLOT_IDX = 0;
-
-    public static final int EL_TIMEOUT = 10;
-
-    public static final boolean EL_SENSOR_PHASE = true;
-
-    public static final double EL_VOLTAGE_RAMP_RATE = 0.1;
-
     public static final boolean EL_INVERT_ONE = true;
     public static final boolean EL_INVERT_TWO = true;
     public static final boolean EL_INVERT_THREE = true;
     public static final boolean EL_INVERT_FOUR = true;
-	public static final boolean EL_INVERT_ROLLER = false;
+    public static final boolean EL_INVERT_ROLLER = false;
+
+
+    //Voltage Limits
+    public static final double DT_VOLTAGE_LIMIT = 11;
+    public static final boolean DT_VOLTAGE_LIMIT_ENABLED = true;
+    public static final double INTAKE_VOLTAGE_LIMIT = 11;
+    public static final boolean INTAKE_VOLTAGE_LIMIT_ENABLED = true;
+    public static final double ARM_VOLTAGE_LIMIT = 11;
+    public static final boolean ARM_VOLTAGE_LIMIT_ENABLED = true;
+    public static final double EL_VOLTAGE_LIMIT = 11;
+    public static final boolean EL_VOLTAGE_LIMIT_ENABLED = true;
+
+    //Current Limits
+    public static final int DT_CURRENT_LIMIT = 30;
+    public static final boolean DT_CURRENT_LIMIT_ENABLED = false;
+    public static final int INTAKE_CURRENT_LIMIT = 30;
+    public static final boolean INTAKE_CURRENT_LIMIT_ENABLED = false;
+    public static final int ARM_CURRENT_LIMIT = 30;
+    public static final boolean ARM_CURRENT_LIMIT_ENABLED = false;
+    public static final int EL_CURRENT_LIMIT = 30;
+    public static final boolean EL_CURRENT_LIMIT_ENABLED = false;
+
+    //PID Slots
+    public static final int DT_LEFT_PIDSLOT_IDX = 0;
+    public static final int DT_RIGHT_PIDSLOT_IDX = 0;
+    public static final int INTAKE_LEFT_PIDSLOT_IDX = 0;
+    public static final int INTAKE_RIGHT_PIDSLOT_IDX = 0;
+    public static final int ARM_LEFT_PIDSLOT_IDX = 0;
+    public static final int ARM_RIGHT_PIDSLOT_IDX = 0;
+    public static final int EL_PIDSLOT_IDX = 0;
+
+    //Timeouts
+    public static final int DT_TIMEOUT = 10;
+    public static final int INTAKE_TIMEOUT = 10;
+    public static final int ARM_TIMEOUT = 10;
+    public static final int EL_TIMEOUT = 10;
+
+    //Sensor Phase
+    public static final boolean DT_LEFT_SENSOR_PHASE = true;
+    public static final boolean DT_RIGHT_SENSOR_PHASE = true;
+    public static final boolean ARM_LEFT_SENSOR_PHASE = true;
+    public static final boolean ARM_RIGHT_SENSOR_PHASE = true;
+    public static final boolean EL_SENSOR_PHASE = true;
+
+    //Ramp Rates
+    public static final double DT_VOLTAGE_RAMP_RATE = 0.1;
+  
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,7 +14,7 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-public static int DT_FRONT_LEFT = 1;
+  public static int DT_FRONT_LEFT = 1;
   public static int DT_REAR_LEFT = 0;
   public static int DT_FRONT_RIGHT = 2;
   public static int DT_REAR_RIGHT = 3;
@@ -36,7 +36,7 @@ public static int DT_FRONT_LEFT = 1;
 
   public static int ARM_SHIFTER_1 = 0;
   public static int ARM_SHIFTER_2 = 0;
-  
+
   public static int ARM_LEFT = 1;
   public static int ARM_RIGHT = 2;
 

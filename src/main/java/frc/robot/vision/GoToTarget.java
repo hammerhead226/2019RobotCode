5fc0d9dcd34cf01f03232af11776268f4f7fbf90
njaison <nidhijaison@gmail.com>
2019-02-07 19:00:35 -0500

    Renamed turn command and add code

--- src/main/java/frc/robot/vision/GoToTarget.java
+++ src/main/java/frc/robot/vision/GoToTarget.java
@@ -1,26 +1,40 @@
 package frc.robot.vision;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 /**
  *
  */
-public class TurnToTarget extends Command {
+public class GoToTarget extends Command {
 
-    public TurnToTarget() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    double initialHeading;
+    double setpoint;
+    double area;
+    boolean isTarget;
+
+    public GoToTarget() {
         requires(Robot.driveTrain);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        initialHeading = Robot.driveTrain.getYaw();
+        setpoint = initialHeading + Limelight.getTargetAngle();
+        area = Limelight.getContourArea();
+        isTarget = Limelight.contourFound();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-
+        System.out.println(isTarget);
+        if (isTarget) {
+            Robot.driveTrain.tankDrive(setpoint/100, setpoint/100);
+        } else {
+            Robot.driveTrain.tankDrive(0, 0);
+        }
     }
 
     // Make this return true when this Command no longer needs to run execute()

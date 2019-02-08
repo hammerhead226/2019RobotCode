f1015d54c9b8b29e4b91c50672ab93b4e901f62c
njaison <nidhijaison@gmail.com>
2019-02-07 19:00:14 -0500

    Update Limelight.java

--- src/main/java/frc/robot/vision/Limelight.java
+++ src/main/java/frc/robot/vision/Limelight.java
@@ -1,9 +1,27 @@
 package frc.robot.vision;
 
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight {
+
+    static NetworkTable getTable() {
+        return NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
     static void start(){
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(0);
+        getTable().getEntry("camMode").setNumber(0);
+    }
+
+    static double getTargetAngle(){
+        return getTable().getEntry("tx").getDouble(0.0);
+    }
+
+    static boolean contourFound(){
+        return getTable().getEntry("tv").getDouble(0.0) == 1;
+    }
+
+    static double getContourArea(){
+        return getTable().getEntry("ta").getDouble(0.0);
     }
 }

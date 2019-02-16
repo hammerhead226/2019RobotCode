108675ec67ffb795715375e02c5db956d644215b
njaison <nidhijaison@gmail.com>
2019-02-16 16:10:19 -0500

    Toggles between driver and processing modes

--- src/main/java/frc/robot/vision/Limelight.java
+++ src/main/java/frc/robot/vision/Limelight.java
@@ -9,19 +9,33 @@ public class Limelight {
         return NetworkTableInstance.getDefault().getTable("limelight");
     }
 
-    static void start(){
+    static void start() {
         getTable().getEntry("camMode").setNumber(0);
     }
 
-    static double getTargetAngle(){
+    static boolean camMode = true;
+
+    static void changeCamMode() {
+        if (camMode) {
+            getTable().getEntry("camMode").setNumber(1);
+            getTable().getEntry("ledMode").setNumber(1);
+            camMode = !camMode;
+        } else {
+            getTable().getEntry("camMode").setNumber(0);
+            getTable().getEntry("ledMode").setNumber(3);
+            camMode = !camMode;
+        }
+    }
+
+    static double getTargetAngle() {
         return getTable().getEntry("tx").getDouble(0.0);
     }
 
-    static boolean contourFound(){
+    static boolean contourFound() {
         return getTable().getEntry("tv").getDouble(0.0) == 1;
     }
 
-    static double getContourArea(){
+    static double getContourArea() {
         return getTable().getEntry("ta").getDouble(0.0);
     }
 }
